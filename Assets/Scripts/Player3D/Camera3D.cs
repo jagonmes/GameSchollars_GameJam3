69cd9878c5efc571fae54b794e87daa7b235186e
jagonmes <65248503+jagonmes@users.noreply.github.com>
2023-10-05 13:39:35 +0200

    Colisiones del mapa provisional/control del jugador y cámara

---
+++ Assets/Scripts/Player3D/Camera3D.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Camera3D : MonoBehaviour
+{
+    //Referencia a la camara
+    [SerializeField] private Transform playerCamera;
+
+    //Sensibilidad en ambos ejes
+    public Vector2 sensitivities;
+
+    //Angulos de rotación para jugador y camara
+    private Vector2 m_XYRotation;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+    }
+
+
+    void Update()
+    {
+            //Leer X e Y del ratón
+            Vector2 mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+            //Calcular rotación
+            m_XYRotation.x -= mouseInput.y * sensitivities.y;
+            m_XYRotation.y += mouseInput.x * sensitivities.x;
+
+            //Limitar la rotación vertical
+            m_XYRotation.x = Mathf.Clamp(m_XYRotation.x, -60f, 60f);
+
+            //Giramos al personaje (horizontal)
+            transform.eulerAngles = new Vector3(0f, m_XYRotation.y, 0f);
+            //Giramos la camara (vertical)
+            playerCamera.localEulerAngles = new Vector3(m_XYRotation.x, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/Player3D/Movement3D.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    //Variables controlables por editor
+    [SerializeField] private float moveSmoothTime;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    
+    //Para cálculos
+    private Vector3 m_CurrentMoveVelocity;
+    private Vector3 m_MoveDampVelocity;
+    private Vector3 m_CurrentForceVelocity;
+    
+    //Controlador
+    private CharacterController m_Controller;
+
+    
+
+    void Start()
+    {
+        //Asignamos el controlador
+        m_Controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        //Leemos y normalizamos la entrada 
+        Vector3 playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        if (playerInput.magnitude > 1f)
+            playerInput.Normalize();
+
+        //Recalculamos la dirección del movimiento
+        Vector3 moveVector = transform.TransformDirection(playerInput);
+
+        //Comprobamos si corremos o no, para elegir la velocidad
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+
+        //Se calcula la velocidad del movimiento
+        m_CurrentMoveVelocity = Vector3.SmoothDamp(m_CurrentMoveVelocity, moveVector * currentSpeed, ref m_MoveDampVelocity, moveSmoothTime);
+
+        //Se asigna -2 a la velocidad horizontal para que se mantenga pegado al suelo
+        m_CurrentForceVelocity.y = -2f;
+
+        //Se mueve al personaje
+        m_Controller.Move(m_CurrentMoveVelocity * Time.deltaTime);
+        //m_Controller.Move(m_CurrentForceVelocity * Time.deltaTime);
+
+    }
+}
