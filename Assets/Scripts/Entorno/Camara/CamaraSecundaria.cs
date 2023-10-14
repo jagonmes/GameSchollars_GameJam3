ecdaab7f96dfce2dd82f45f2beccb9d5dc987590
jagonmes <65248503+jagonmes@users.noreply.github.com>
2023-10-14 12:46:48 +0200

    Implementada Pantalla/Cámara Secundaria
    Se ha implementado un material para hacer de pantalla, así como una segunda cámara que renderiza a una textura (que posteriormente se usa para sacar esa imagen por la pantalla del PC). La segunda cámara tienen un script para redimensionar adecuadamente la RenderTexture  asignada a la misma,

---
+++ Assets/Scripts/Entorno/Camara/CamaraSecundaria.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camarasecundaria : MonoBehaviour
+{
+    
+    [SerializeField]private Camera camara;
+    private RenderTexture rt;
+    private int UltimoAltoDePantalla;
+    private int UltimoAnchoDePantalla;
+    
+    void Start()
+    {
+        rt = camara.targetTexture;
+        UltimoAltoDePantalla = Screen.height;
+        UltimoAnchoDePantalla = Screen.width;
+        CambiarTamañoRT(rt, UltimoAnchoDePantalla, UltimoAltoDePantalla);
+    }
+    
+    void Update()
+    {
+        if (Screen.height != UltimoAltoDePantalla || Screen.width != UltimoAnchoDePantalla)
+        {
+            UltimoAltoDePantalla = Screen.height;
+            UltimoAnchoDePantalla = Screen.width;
+            CambiarTamañoRT(rt, UltimoAnchoDePantalla, UltimoAltoDePantalla);
+        }
+    }
+
+    void CambiarTamañoRT(RenderTexture rt, int ancho, int alto)
+    {
+       rt.Release();
+       rt.width = ancho;
+       rt.height = alto;
+       camara.targetTexture = rt;
+    }
+
+}
