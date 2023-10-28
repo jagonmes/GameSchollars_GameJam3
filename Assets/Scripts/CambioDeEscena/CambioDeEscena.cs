e53d58788e340d6df048fcc20a0196d87b470a15
JesusBL24 <jesusbaslop24@gmail.com>
2023-10-28 13:54:16 +0200

    Dormir peticiones

--- Assets/Scripts/CambioDeEscena/CambioDeEscena.cs
+++ Assets/Scripts/CambioDeEscena/CambioDeEscena.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,28 +10,69 @@ public class CambioDeEscena : MonoBehaviour
 {
     [SerializeField] private int indice = -1;
     [SerializeField] private float tiempo = 3.0f;
-    [SerializeField] private String textoAMostrar;
+
+
+    private String textoAMostrar1 = "Debes comer antes de dormir";
+    private String textoAMostrar2 = "Debes jugar antes de dormir";
+    private String textoAMostrar3 = "Debes jugar y comer antes de dormir";
     [SerializeField] private Fade fade;
 
     public bool haComido = false;
+    private bool haJugado = true;
+
+    //Campo de texto donde se mostrara el texto del objeto
+    private TextMeshProUGUI Text;
+
+    private void Start()
+    {
+       Text = GameObject.Find("TextoSubtitulos").GetComponent<TextMeshProUGUI>();
+    }
     public void cambiarDeEscena()
     {
-        if (haComido)
-        {
-            fade.activar();
-            if (indice == -1)
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            else
-                SceneManager.LoadScene(indice);
-        }
-        else
-        {
-            Debug.Log("No puedo dormir");
-        }
+
+        if (indice == -1)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            
     }
 
     public void invocarCambiarDeEscena()
-    {     
-        Invoke("cambiarDeEscena", tiempo);       
+    {
+        if (haComido && haJugado)
+        {
+            fade.activar();
+            Invoke("cambiarDeEscena", tiempo);
+        }
+        else
+        {
+            noCambiarEscena();
+        }
+    }
+
+    public void activarJugado()
+    {
+        haJugado = true;
+    }
+
+    private void QuitaTexto()
+    {
+        Text.text = null;
+    }
+
+    private void noCambiarEscena()
+    {
+
+        if (!haComido && haJugado)
+        {
+            Text.text = textoAMostrar1;
+        }
+        else if (haComido && !haJugado)
+        {
+            Text.text = textoAMostrar2;
+        }
+        else
+        {
+            Text.text = textoAMostrar3;
+        }
+         Invoke("QuitaTexto", 2f);
     }
 }
